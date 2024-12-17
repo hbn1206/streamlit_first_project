ff9168dd5c6249f7a0218d5fd4a26b6a68d74c21
hbn1206 <73619838+hbn1206@users.noreply.github.com>
2024-12-17 20:58:40 +0900

    Update 02_인구구조그리기.py

--- pages/02_인구구조그리기.py
+++ pages/02_인구구조그리기.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import koreanize_matplotlib
+# import koreanize_matplotlib
 from matplotlib import font_manager, rc
 
 # 한글 폰트 설정
